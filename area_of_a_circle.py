b82450bb0015fb82197b305058194552319d7d17
Phyllis <phyllistockton@gmail.com>
2025-04-09 17:05:42 -0500

    'area_of_a_circle.py'

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -12,5 +12,9 @@ area2 = compute_area_of_circle(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
 
 radius3 = 2500
-area3 = compute_area_of_circle(area2)
+area3 = compute_area_of_circle(radius3)
 print(f"The area of circle with radius {radius3} is: {area3}")
+
+radius4 = 15
+area4 = compute_area_of_circle(radius4)
+print(f"The area of circle with radius {radius4} is: {area4}")
